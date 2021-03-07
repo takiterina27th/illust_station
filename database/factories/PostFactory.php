51df511060d229f30e2bf4f0d47598f1b382fa63
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 18:19:07 +0900

    作成するダミーデータの詳細を記述
    詳細：$faker->で条件をそれぞれ設定した。

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->realText(50),
+        'content' => $faker->realText(200),
+        'user_id' => $faker->randomElement(['1', '2']),
     ];
 });
