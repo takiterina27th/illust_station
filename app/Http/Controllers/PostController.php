c1446d45aa1fd15ed4ef898d11e9488d5ce41efa
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 12:13:52 +0900

    compact関数で変数をビューに渡す

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,7 +75,7 @@ class PostController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
-        return view('posts.edit');
+        return view('posts.edit', compact('post'));
     }
 
     /**
