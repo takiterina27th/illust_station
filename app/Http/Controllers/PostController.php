28acad4a02187db2d1fe20fd4abe91ab6223c229
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 13:23:51 +0900

    storeにバリデーションがかかるように設定

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -41,7 +41,7 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePost $request)
     {
         $post = new Post;
 
