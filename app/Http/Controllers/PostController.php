5b7aaa908e32da55ac94911b68ceaff609537789
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 12:26:43 +0900

    コントローラーで処理を記述
    詳細：新しくインスタンスを作成するのではなくすでにあるものを取得し、上書きする。

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,7 +87,15 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $post = Post::find($id);
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->user_id = Auth::user()->id;
+
+        $post->save();
+
+        return redirect('/');
     }
 
     /**
