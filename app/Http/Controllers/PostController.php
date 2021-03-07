6d2b01fd43f596f76c990b301dedf9558daf4d29
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 13:22:03 +0900

    フォームリクエストクラスをコントローラーで読み込み

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StorePost;
 
 class PostController extends Controller
 {
