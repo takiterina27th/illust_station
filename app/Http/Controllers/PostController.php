18354153b7e1ffe31e596599b1ddade14607fd41
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-07 13:33:28 +0900

    編集ページにもバリデーションがかかるように設定した

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -86,7 +86,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StorePost $request, $id)
     {
         $post = Post::find($id);
 
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -14,6 +14,16 @@
                         </div>
                     @endif
 
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                      <ul class="mb-0">
+                        @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                        @endforeach
+                      </ul>
+                    </div>
+                    @endif
+
                     <form method="POST" action="{{ route('posts.update', ['id' => $post->id ])}}">
                     @csrf
 
