be03ba0678db369009ccdf0d2b246c17c4434dd6
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-08 19:44:21 +0900

    migrationファイルの編集
    詳細：up() にはカラムの追加を、down() はこの処理を巻き戻す処理を記述した。

--- database/migrations/2021_03_08_194115_add_image_to_posts_table.php
+++ database/migrations/2021_03_08_194115_add_image_to_posts_table.php
@@ -14,7 +14,7 @@ class AddImageToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->string('image')->after('content');
         });
     }
 
@@ -26,7 +26,7 @@ class AddImageToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('image'); 
         });
     }
 }
