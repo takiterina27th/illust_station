d9448c3cd235bd9c192b46d2776b4b46c51beedb
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-09 17:00:39 +0900

    ユーザー詳細ページ及び編集ページを返すようにコントローラーに記述

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+    public function show($id) {
+
+        return view('users.show');
+
+    }
+    
+    public function edit() {
+
+        return view('users.edit');
+
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
