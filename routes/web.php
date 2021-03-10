dd11145b56782c350708710cbc37458530d9da2a
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-03-10 19:39:58 +0900

    userにstoreアクションは不要であったため削除

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,6 @@ Route::group(['prefix' => 'posts', 'middleware' => 'auth'], function(){
 Route::group(['middleware' => 'auth'], function()
 {  
   Route::get('show/{id}', 'UserController@show')->name('users.show');
-  Route::post('store', 'UserController@store')->name('users.store');
   Route::get('edit/{id}', 'UserController@edit')->name('users.edit');
   Route::post('update/{id}', 'UserController@update')->name('users.update');
   Route::post('destroy/{id}', 'UserController@destroy')->name('users.destroy');
