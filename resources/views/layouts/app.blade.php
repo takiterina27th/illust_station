fdd99ff411fef75227a9b89752a8ccf5876d13c3
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-05-08 17:00:07 +0900

    ロゴのS3にあるオブジェクトURLを設定
    詳細：ロゴ画像をstorageディレクトリ配下に置いていると本番環境で表示されないので、S3のバケットに画像をアップロードしてそのオブジェクトURLを記述し本番環境で画像を表示できるように設定した。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand ml-1" href="{{ url('/') }}">
-                    <img src="/storage/logo_middle_size.png" class="img-fluid w-50 h-auto">
+                    <img src="https://illust-station-takiterina-bucket.s3-ap-northeast-1.amazonaws.com/logo_middle_size.png" class="img-fluid w-50 h-auto" alt="ロゴ">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
