9d83b9b173f4a93567c7a54712e0006e0e664303
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-05-08 13:15:47 +0900

    ロゴを追加した
    詳細：画像ファイルでロゴを追加し、マージンを調整した。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand ml-1" href="{{ url('/') }}">
+                    <img src="/storage/logo_middle_size.png" class="img-fluid w-50 h-auto">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
