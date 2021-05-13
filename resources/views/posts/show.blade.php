36bd03400a4ccdf981755fc6848e4bcb4f764d84
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-05-13 10:01:47 +0900

    show.blade.phpのボタンデザインを変更した
    詳細：
    変更ボタンはbtn-infoクラス、削除ボタンはbtn-outline-dangerクラスを指定した

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -38,12 +38,12 @@
                     　@if(($post->user_id) === (Auth::user()->id ))
                       <div class="d-flex">
                         <a href="{{ route('posts.edit', ['id' => $post->id])}}">
-                          <input class="btn btn-primary" type="submit" value="変更する">
+                          <input class="btn btn-info" style="color: #f8f9fa;" type="submit" value="変更する">
                         </a>
                         <div class="ml-3">
                           <form method="POST" action="{{ route('posts.destroy', ['id' => $post->id])}}" id="delete_{{ $post->id}}">
                             @csrf
-                            <a href="#" class="btn btn-danger" data-id="{{ $post->id }}" onclick="deletePost(this); ">削除する</a>
+                            <a href="#" class="btn btn-outline-danger" data-id="{{ $post->id }}" onclick="deletePost(this); ">削除する</a>
                           </form>
                         </div>
                       </div>
