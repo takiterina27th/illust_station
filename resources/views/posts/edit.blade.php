7f5c17d28b8394bf039cbf987a178b046baddd44
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-05-28 19:43:53 +0900

    編集ページのタイトルを変更
    詳細：テキストとfont-weightを変更した。

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">情報を編集しよう！</div>
+                <div class="card-header font-weight-bold">編集しよう！</div>
 
                 <div class="card-body">
                     @if (session('status'))
