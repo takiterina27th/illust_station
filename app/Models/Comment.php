e9f8380d0984a1fa43fa10ff8035b97f384a9dd1
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-05-29 12:02:39 +0900

    Commentモデルにリレーションを設定
    詳細：commentテーブルはuserテーブル及びpostテーブルそれぞれに対して１対多の関係にあるのでそれぞれのモデルにリレーションを記述した。
    また、特定のカラムに$fillable設定をした。

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['user_id', 'post_id', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models/Post');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function comment()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
 }
