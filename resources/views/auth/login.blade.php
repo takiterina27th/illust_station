1bdf5cb16bf3e8f6993f6caeb28a2a06a3c33f6c
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-06-10 21:56:54 +0900

    ログインページのデザインを変更した。

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,74 +3,55 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header text-center font-weight-bold">{{ __('ログイン') }}</div>
+        <div class="col-md-4 login-form">
+            <div class="login-form__logo-box">
+                <div class="login-form__logo">
+                    <img src="https://illust-station-takiterina-bucket.s3-ap-northeast-1.amazonaws.com/logo_middle_size.png" class="img-fluid" alt="ロゴ">
+                </div>
+                <div class="login-form__catchphrase">好きを描く</div>
+            </div>
+            <div class="login-form__form-box">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                        <div class="col-md-12 login-form__field">
+                            <input id="email" type="email" class="form-control login-form__input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="メールアドレス">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                        <div class="col-md-12 login-form__field">
+                            <input id="password" type="password" class="form-control login-form__input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="パスワード">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                        
+                        <button type="submit" class="btn font-weight-bold auth-button" style="background-color: #295d72;">
+                            {{ __('ログイン') }}
+                        </button>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('記憶する') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn font-weight-bold button-design">
-                                    {{ __('ログインする') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" style="color: #295d72;" href="{{ route('password.request') }}">
-                                        {{ __('パスワードを忘れた方はこちら') }}
-                                    </a>
-                                @endif
-                            </div>
-                            <div class="col-md-8 offset-md-4 mt-3">
-                                <a href="/login/twitter">
-                                    <button type="button" class="btn font-weight-bold button-design">Twiiterログイン</button>
+                        @if (Route::has('password.request'))
+                            <div class="login-form__reset-box">
+                                <a class="btn btn-link login-form__reset-link" style="color: #295d72;" href="{{ route('password.request') }}">
+                                    {{ __('パスワードがわからない') }}
                                 </a>
                             </div>
+                        @endif
+                        <div class="login-form__social">
+                            <a href="/login/twitter" class="login-form__social-link">
+                                <button type="button" class="btn font-weight-bold auth-button"style="background-color: #0096fa;">Twiiterで続ける</button>
+                            </a>
                         </div>
-                    </form>
-                </div>
+                        <div class="login-form__recaptcha-terms">
+                            This site is protected by Akimine
+                        </div>
+                </form>
             </div>
         </div>
     </div>
