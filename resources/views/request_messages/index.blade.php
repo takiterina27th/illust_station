c52e9acdebe8d1243e3f53d44a7d8bf3850e98d0
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-06-28 01:17:51 +0900

    不要なコメントアウトを削除

--- resources/views/request_messages/index.blade.php
+++ resources/views/request_messages/index.blade.php
@@ -1,40 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <table>
-    <tr>
-        <th>タイトル</th>
-        <th>メッセージ</th>
-        <th>送信者</th>
-    </tr>
-@foreach($requestsToUser as $request)
-    <tr>
-        <td>{{ $request->title }}</td>
-        <td>{{ $request->body }}</td>
-        <td>{{ $request->fromUser->name }}</td>
-    </tr>
-@endforeach
-</table> -->
-
-
-<!-- <br><br>
-
-送信済みメッセージ
-<table>
-    <tr>
-        <th>タイトル</th>
-        <th>メッセージ</th>
-        <th>宛先</th>
-    </tr>
-@foreach($requestsFromUser as $request)
-    <tr>
-        <td>{{ $request->title }}</td>
-        <td>{{ $request->body }}</td>
-        <td>{{ $request->toUser->name }}</td>
-    </tr>
-@endforeach
-</table> -->
-
 <div class="request-tab col-md-8">
     <input id="receive_request" type="radio" name="tab_item" checked>
     <label class="request-tab__item" for="receive_request">受信リクエスト</label>
