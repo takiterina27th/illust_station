3b08bcd0432ba845cccd7b9d1ae91f2d63b73f7b
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-06-27 11:15:13 +0900

    request_messageコントローラーにリクエストを保存する処理を記述した
    詳細：投稿詳細ページのフォームからリクエストを受け取り、その情報をrequest_messagesテーブルに保存する。
    その後リクエストを送った投稿詳細ページにリダイレクトする。

--- app/Http/Controllers/Request_messageController.php
+++ app/Http/Controllers/Request_messageController.php
@@ -3,7 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Models\Request_message;
 
 class Request_messageController extends Controller
 {
+    public function store(Request $request)
+    {
+        $post_id = $request->post_id;
+
+        $message = new Request_message();
+        $message->title = $request->title;
+        $message->body = $request->body;
+        $message->to_user_id = $request->to_user_id;
+        $message->from_user_id = auth()->id();
+        $message->save();
+        
+        return redirect()->route('posts.show', ['id' => $post_id ]);
+    }
 }
