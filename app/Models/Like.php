554d6e2a22413d07f4c0805a90c7b94636fb9f40
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-08-07 23:57:08 +0900

    Like.phpにリレーションを記述
    詳細：like_existメソッドはいいねがすでにされているかどうか確認するメソッドである。

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Models\Post');
+    }
+
+    public function like_exist($id, $post_id)
+    {
+        $exist = Like::where('user_id', '=', $id)->where('post_id', '=', $post_id)->get();
+
+        if (!$exist->isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
