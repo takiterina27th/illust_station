e1bff149e5a94f857f2a96d1b0c42f55812a591d
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-08-08 19:50:45 +0900

    投稿詳細ページにいいねマークを記述
    詳細：機能に問題がないか確認するためにとりあえず簡易的にviewを記述した。

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -42,6 +42,18 @@
                         <span class="ml-3">投稿日：</span>{{ $post->created_at}}
                         </div>
 
+                        @if($like_model->like_exist(Auth::user()->id,$post->id))
+                            <p class="favorite-marke">
+                                <a class="js-like-toggle loved" href="" data-postid="{{ $post->id }}"><i class="fas fa-heart"></i></a>
+                                <span class="likesCount">{{$post->likes_count}}</span>
+                            </p>
+                        @else
+                            <p class="favorite-marke">
+                                <a class="js-like-toggle" href="" data-postid="{{ $post->id }}"><i class="fas fa-heart"></i></a>
+                                <span class="likesCount">{{$post->likes_count}}</span>
+                            </p>
+                        @endif​
+
                         <div class="mt-2">
                             @foreach($post->tag as $post_tag)
                                 <a href="{{ route('tags.show', ['id' => $post_tag->id]) }}">
