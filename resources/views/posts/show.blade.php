d8384fdd7c900348c1d8dacae131e20b8594da66
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-08-15 19:32:02 +0900

    ログインしているユーザーにしかいいね!ボタンを押せないようにする

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -41,7 +41,7 @@
                         <div class="border-bottom pt-2 pb-2">
                         <span class="ml-3">投稿日：</span>{{ $post->created_at}}
                         </div>
-
+                        @auth
                         @if($like_model->like_exist(Auth::user()->id,$post->id))
                             <p class="favorite-marke">
                                 <a class="js-like-toggle loved" href="" data-postid="{{ $post->id }}"><i class="fas fa-heart"></i></a>
@@ -53,6 +53,7 @@
                                 <span class="likesCount">{{$post->likes_count}}</span>
                             </p>
                         @endif​
+                        @endauth
 
                         <div class="mt-2">
                             @foreach($post->tag as $post_tag)
